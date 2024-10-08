7107017ca0d3f357a30def8bdc14f7a99ef114c7
Manoj Joshi <manojj@redapt.com>
2024-10-07 21:53:25 -0700

    ai-service code

--- Labfiles/00-mero/First.py
+++ Labfiles/00-mero/First.py
@@ -1,2 +1,11 @@
+import pyspark
+import pandas as pd
+
 msg = "\nHellow World, \nWe will have fun soon.\n"
 print(msg)
+
+pd.read_csv('Data.csv')
+
+from pyspark.sql import SparkSession
+spark=SparkSession.builder.appName('Test').getOrCreate()
+df_datafile = spark.read.csv('Data.csv');
